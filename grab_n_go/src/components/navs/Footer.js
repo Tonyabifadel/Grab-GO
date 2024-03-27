dbb023c0e2fe6325bd08faaca99fcfa510ac2cc0
DanyAbboud04 <Danymabboud@gmail.com>
2024-03-27 15:40:57 +0200

    fixed footer and menu for footer and all products

--- grab_n_go/src/components/navs/Footer.js
+++ grab_n_go/src/components/navs/Footer.js
@@ -1,17 +1,19 @@
 function Footer(){
     return (
         <>
-            <div className="container-foot">
-                <h1>Naturals</h1>
-                <ul className="logo-ul">
-                    <li><a href="https://www.instagram.com/naturals.lb/?igsh=OGQ5ZDc2ODk2ZA%3D%3D"><img src="/images/insta_logo.png" alt="Instagram" /></a></li>
-                    <li><a href="https://www.facebook.com/profile.php?id=100091449817954&mibextid=LQQJ4d"><img src="/images/fcb_logo.png" alt="Facebook" /></a></li>
-                    <li><a href="https://www.tiktok.com/@naturals.lb?_t=8jzVjmCHk5R&_r=1"><img src="/images/tiktok_logo.png" alt="TikTok" /></a></li>
-                    <li><a href="mailto:your_email@example.com"><img src="/images/mail_logo.jpg" alt="Mail" /></a></li>
-                    <li><a href="https://wa.me/76732328"><img src="/images/wstp_logo.png" alt="WhatsApp" /></a></li>
-                </ul>
-                <p>All rights reserved ©2024 - Organic & Natural Soaps</p>
-            </div>
+            <footer>
+                <div className="container-foot">
+                    <h1>Naturals</h1>
+                    <ul className="logo-ul">
+                        <li><a href="https://www.instagram.com/naturals.lb/?igsh=OGQ5ZDc2ODk2ZA%3D%3D"><img src="/images/insta_logo.png" alt="Instagram" /></a></li>
+                        <li><a href="https://www.facebook.com/profile.php?id=100091449817954&mibextid=LQQJ4d"><img src="/images/fcb_logo.png" alt="Facebook" /></a></li>
+                        <li><a href="https://www.tiktok.com/@naturals.lb?_t=8jzVjmCHk5R&_r=1"><img src="/images/tiktok_logo.png" alt="TikTok" /></a></li>
+                        <li><a href="mailto:your_email@example.com"><img src="/images/mail_logo.jpg" alt="Mail" /></a></li>
+                        <li><a href="https://wa.me/76732328"><img src="/images/wstp_logo.png" alt="WhatsApp" /></a></li>
+                    </ul>
+                    <p>All rights reserved ©2024 - Organic & Natural Soaps</p>
+                </div>
+            </footer>
         </>
     )
 }
