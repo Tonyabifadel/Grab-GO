dfb911ad3fe538aa8cdbe6ca109342880a43d502
DanyAbboud04 <Danymabboud@gmail.com>
2024-03-27 15:50:00 +0200

    added styles to all products

--- grab_n_go/src/components/All_Products.js
+++ grab_n_go/src/components/All_Products.js
@@ -1,11 +1,17 @@
 import Navbar from "./navs/Navbar";
 import Head from "./navs/Head";
+import Footer from './navs/Footer'
+import styles from '../styles/All_Products.module.css'
 
 function All_products(){
     return(
         <>
             <Head />
             <Navbar />
+            <div className={styles.title}>
+                All Products
+            </div>
+            <Footer />
         </>
     )
 }
